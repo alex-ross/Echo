1f58b3925ea371ef9aa102cb87993a34f8dd6b02
Alexander Ross <alex@aross.se>
2018-07-30 19:32:22 +0200

    Implementing the business logic

--- Package.swift
+++ Package.swift
@@ -1,12 +1,40 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Echo",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .executable(
+            name: "EchoServer",
+            targets: ["EchoServer"]
+        ),
+        .executable(
+            name: "EchoClient",
+            targets: ["EchoClient"]
+        ),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/grpc/grpc-swift.git",
+            from: "0.4.1"
+        ),
+    ],
+    // For each target, Swift Package Manager will look for sources under the
+    // `Sources/<TargetName>` directory
+    targets: [
+        .target(
+            name: "EchoServer",
+            dependencies: [
+                "SwiftGRPC"
+            ]
+        ),
+        .target(
+            name: "EchoClient",
+            dependencies: [
+                "SwiftGRPC"
+            ]
+        ),
+    ]
 )
